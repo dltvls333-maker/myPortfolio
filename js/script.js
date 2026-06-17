f2215968d731166f0ddedc4b4f5442563eff5918
your name <dltvls333@gmail.com>
2026-06-17 19:48:08 +0900

    로고수정

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-/**
- * 박종광 포트폴리오 메인 스크립트 인터랙션 엔진
- * * - Swiper Slider 인스턴스 초기화 및 루프 처리
- * - 마우스 호버 시 일시 정지 활성화 (pauseOnMouseEnter)
- */
+
 document.addEventListener('DOMContentLoaded', () => {
     
     const swiper = new Swiper('.swiper_portfolio', {
